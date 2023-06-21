f3e8044aa19670ff1c502e26b06ce2262d8ccdfd
mehdi <iran.mehdi2005@gmail.com>
2023-06-21 18:33:54 +0330

    Squashed 'service2/' content from commit 97df303
    git-subtree-dir: service2
    git-subtree-split: 97df303c2780aeee896b73d228afeed5de9da643

---
+++ src/main/java/com/example/microservice/controller/PersonController.java
@@ -0,0 +1,19 @@
+package com.example.microservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mahdi Mohammadi, 5/26/2023 9:08 PM
+ */
+@RestController
+public class PersonController {
+
+/*    @GetMapping("get-service2")
+    public Person getPerson() {
+        System.out.println("service2");
+    }*/
+
+}
+
